23342f3da9a7d55c72bbe963e9acd1eef487ccc3
AleksandraVIA <293687@via.dk>
2024-02-29 13:47:34 +0100

    Test made

--- assignment1/src/model/VinylLibraryModelManager.java
+++ assignment1/src/model/VinylLibraryModelManager.java
@@ -2,30 +2,36 @@ package model;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
 import java.util.List;
 
-public class VinylLibraryModelManager implements VinylLibraryModel
-{
+public class VinylLibraryModelManager implements VinylLibraryModel {
   private List<Vinyl> vinylList;
   private PropertyChangeSupport propertyChangeSupport;
-  @Override public List<Vinyl> getAllVinyls()
-  {
+
+  public VinylLibraryModelManager() {
+    this.vinylList = new ArrayList<>();
+    this.propertyChangeSupport = new PropertyChangeSupport(this);
+  }
+
+  @Override
+  public List<Vinyl> getAllVinyls() {
     return vinylList;
   }
 
-  @Override public void addVinyl(Vinyl[] vinyl)
-  {
-    for (Vinyl vinyls : vinylList) {
+  @Override
+  public void addVinyl(Vinyl[] vinyl) {
+    for (Vinyl vinyls : vinyl) {
       vinylList.add(vinyls);
-      firePropertyChange("VinylAdded", null, vinyl);
+      firePropertyChange("VinylAdded", null, vinyls);
     }
   }
 
-  @Override public void removeVinyl(String title)
-  {
+  @Override
+  public void removeVinyl(String title) {
     Vinyl vinylToRemove = null;
     for (Vinyl vinyl : vinylList) {
-      if (vinyl.getTitle().equals(vinyl.getTitle())) {
+      if (vinyl.getTitle().equals(title)) {
         vinylToRemove = vinyl;
         break;
       }
@@ -36,18 +42,17 @@ public class VinylLibraryModelManager implements VinylLibraryModel
     }
   }
 
-  public void firePropertyChange(String propertyName, Object oldValue, Object newValue)
-  {
+  public void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
     propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
   }
 
-  @Override public void addListener(PropertyChangeListener listener)
-  {
+  @Override
+  public void addListener(PropertyChangeListener listener) {
     propertyChangeSupport.addPropertyChangeListener(listener);
   }
 
-  @Override public void removeListener(PropertyChangeListener listener)
-  {
+  @Override
+  public void removeListener(PropertyChangeListener listener) {
     propertyChangeSupport.removePropertyChangeListener(listener);
   }
 }
